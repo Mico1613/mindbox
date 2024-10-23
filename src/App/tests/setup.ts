cc230edad008268b80abaef0b9fc5f997543d2b3
vadim <magnalov@mail.ru>
2024-10-23 06:56:14 +0300

    final (i hope)

--- src/App/tests/setup.ts
+++ src/App/tests/setup.ts
@@ -1,4 +1,8 @@
 import { expect } from "vitest";
 import * as matchers from "@testing-library/jest-dom/matchers";
 
+// Костыль, чтобы тесты не падали из-за использования функции scrollTo в компоненте
+// (в либе jsdom почему-то не имплементирован ряд методов, доступных в браузере)
+Element.prototype.scrollTo = () => null;
+
 expect.extend(matchers);
